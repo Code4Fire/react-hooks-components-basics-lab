c06709f2933301875af82004e59923e2c4805a92
Tiffany_Tam <tamtiffany89@gmail.com>
2021-11-01 11:10:11 -0400

    commit tree 78a4910a88554f5f92502567191deec5b8e1b3f8

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,9 @@
 import React from "react";
+import NavBar from "./NavBar";
+import About from "./About";
+import Home from "./Home";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
 {/* write an <About> component here */}
 
@@ -23,8 +12,9 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
+      
     </div>
   );
 }
